074df008990f2f01fc6dedcb312b4722931b26a4
AprilBlossoms <AprilBlossoms@users.noreply.github.com>
2021-02-11 03:21:34 -0800

    Style Changes and Progress Bar
    Added progress bar at the top with a rocket image that moves with each correct answer, and a "you won" screen once the rocket reaches the planet image.
    Added a galaxy background and fixed the spacing issues.

--- app.js
+++ app.js
@@ -16,26 +16,32 @@ const hints = ["images/one.png", "images/two.png", "images/three.png", "images/f
 const plus = {string:"+", img:"images/plus.png"}
 const minus = {string:"-", img:"images/minus.png"}
 const operations = [plus, minus]
+const correctResult = ['Way to go!', 'Great job!', 'Congratulations, that\'s right!', 'Woohoo!', 'Awesome!']
+const wrongResult = ['Sorry, try again', 'So close!', 'Not quite', 'Almost!']
 var op;
 var nums;
-var streak = 0;
+var playerName;
+
 
 function start() {
-  document.querySelector(".start").style.display = "none";
+  playerName = document.getElementsByName("playerName")[0].value;
+  document.querySelector(".fullPage").style.display = "none";
   generateProblem();
 }
+
 
 function generateProblem() {
   op = operations[Math.floor(Math.random()*operations.length)]; //grabs plus or minus
   nums = [x, y];
   nums.sort(function(a, b){return a - b}); //sorts the numbers so I can get the bigger one first
-  document.getElementById("equation").innerHTML = `${nums[1]} ${op.string} ${nums[0]} = ` //print the equation
+  document.getElementById("equation").innerHTML = `${nums[1]} ${op.string} ${nums[0]} = `;//print the equation
 }
+
+var distance = 0;
 
 function calculate() { //when Enter is clicked
   var answer = document.getElementsByName("answer")[0].value; //gets the submitted answer
   var correctAns;
-  //set correct answer, depending on the operation
   if (op == plus) {
     correctAns = sum(x, y)
   } else {
@@ -43,29 +49,34 @@ function calculate() { //when Enter is clicked
   }
 
   if (answer == correctAns) {
-    document.getElementById("result").innerHTML = "Congratulations, that is correct!";
-    //hide the enter button and show the reset button
-    document.getElementById("nextRound").style.display = "inline-block";
-    document.getElementById("enter").style.display = "none";
-    streak++ //increase the streak counter
+    document.getElementById("result").innerHTML = correctResult[Math.floor(Math.random()*correctResult.length)];
+    document.querySelector(".hint").style.display = "none";
+    distance += 85;
   }
   else {
-    document.getElementById("result").innerHTML = `So close! Try again`;
-    streak = 0 //reset streak to 0
+    document.getElementById("result").innerHTML = wrongResult[Math.floor(Math.random()*wrongResult.length)];
+    if(distance > 0){
+    distance -= 85;
+  }
+  }
+  document.getElementById("rocket").style.left=`${distance}px`
+
+  if (distance == 850) {
+  document.getElementById("success").style.display="block";
+  document.getElementById("name").innerHTML="Congratulations "+ playerName
+  }
+  document.getElementById('answer').value = '' //empty the form
+
+  if (answer == correctAns) {
+    nextRound();
   }
 }
 
-function nextRound() { //when nextRound is clicked
-  document.getElementById('answer').value = '' //empty the form
-  document.getElementById("result").innerHTML = ""; //emptys the result space
-  document.getElementById("nextRound").style.display = "none"; //hides nextRound button
-  document.getElementById("enter").style.display = "inline-block"; //show the enter button
-  document.getElementById("hint").style.display = "inline-block"; //show the hint button
-  //reset x and y
+function nextRound() {
   x = Math.floor((Math.random() * 7) + 1);
   y = Math.floor((Math.random() * 7) + 1);
-  document.querySelector(".hint").style.display = "none"; //hide hint display
-  generateProblem(); //create a new equation
+  document.getElementById("hint").style.display = "inline-block";
+  generateProblem();
 }
 
 function showHint() {
@@ -78,5 +89,14 @@ function showHint() {
   document.getElementById("x").src = hints[nums[1] - 1];
   document.getElementById("op").src = op.img;
   document.getElementById("y").src = hints[nums[0] - 1];
+}
+
+function newGame() {
+  playerName = "";
+  distance = 0;
+document.getElementById("rocket").style.left=`${distance}px`
+  document.getElementById("startPage").style.display = "block";
+  document.getElementById("success").style.display = "none";
+  nextRound();
 
 }
